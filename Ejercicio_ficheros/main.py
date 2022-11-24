718a21ad43fb11e027aa0080a6f90cb929494671
Alvaro Lopez <alvaro.lopez.1990@gmail.com>
2022-11-24 09:13:20 +0100

    Añadiendo split a las asignaturas

--- Ejercicio_ficheros/main.py
+++ Ejercicio_ficheros/main.py
@@ -10,10 +10,7 @@ if __name__ == '__main__':
         for valor in valores:
             colegio = clases.Colegio(f"{valor[0]}")
             alumno = clases.Alumno(f"{valor[1]}", f"{valor[2]}", f"{valor[3]}")
-            asignatura = clases.Asignatura([valor[4]])
+            asignatura = clases.Asignatura(valor[4].split(";"))
             #Creando archivos a partir de los objetos
             with open(f'{valor[0]}.txt','w', encoding='utf8') as archivo:
                 archivo.write(f"{valor}|\n")
-
-
-
